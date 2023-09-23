a8f7efa455517be116da7cb2ac3493cb34079a7f
Hassan Fathy <hassanfathy460@gmail.com>
2023-09-23 21:40:48 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { Fragment } from 'react';
 import { Container } from 'react-bootstrap';
+import SideBar from './Component/SideBar';
 
 function App() {
   return (
     <Fragment>
       <Container>
-
+        <SideBar />
       </Container>
     </Fragment>
   );
--- src/Component/SideBar.js
+++ src/Component/SideBar.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Container } from 'react-bootstrap'
+import * as Unicons from '@iconscout/react-unicons';
 
 export default function SideBar() {
     return (
         <Container>
             <article className='row'>
                 <article className='col-lg-3 d-flex'>
-                    <ul>
-                        <li></li>
+                    <ul className=' '>
+                        <li><Unicons.UilBars /></li>
                         <li></li>
                         <li></li>
                     </ul>
