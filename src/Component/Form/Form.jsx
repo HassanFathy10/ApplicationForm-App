274d990235cab466974555007c6b37539a331a31
Hassan Fathy <hassanfathy460@gmail.com>
2023-09-25 01:56:57 +0300

    end ui design

--- src/Component/Form/Form.jsx
+++ src/Component/Form/Form.jsx
@@ -1,14 +1,11 @@
 import React from 'react'
-import Tab from 'react-bootstrap/Tab';
-import Tabs from 'react-bootstrap/Tabs';
-import Styles from './styles.module.css'
+import { Nav } from 'react-bootstrap';
 import PhotoForm from '../AppForm/PhotoForm';
 import PersonalForm from '../AppForm/PersonalForm';
-import * as Unicons from '@iconscout/react-unicons';
-
-import { Nav} from 'react-bootstrap';
 import ProfileForm from '../AppForm/ProfileForm';
 import AdditionalForm from '../AppForm/AdditionalForm';
+import * as Unicons from '@iconscout/react-unicons';
+import Styles from './styles.module.css'
 
 
 export default function Form() {
@@ -18,31 +15,32 @@ export default function Form() {
             <article className='sidebar col-lg-1 shadow'>
                 <Nav className={Styles.SideBar}>
                     <Nav.Item>
-                        <Nav.Link className='m-3 mt-3 mb-5' href="#home"><Unicons.UilBars /></Nav.Link>
+                        <Nav.Link className='m-4 mt-3 mb-5' href="#home"><Unicons.UilBars /></Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                        <Nav.Link className='m-3' href="#about"><Unicons.UilHome /></Nav.Link>
+                        <Nav.Link className='m-4' href="#about"><Unicons.UilHome /></Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                        <Nav.Link className='m-3' href="#services"><Unicons.UilClipboardNotes /></Nav.Link>
+                        <Nav.Link className='m-4' href="#services"><Unicons.UilClipboardNotes /></Nav.Link>
                     </Nav.Item>
                 </Nav>
             </article>
             <article className='col-lg-10 col-md-10 col-sm-10 mt-5 '>
                 <article className='col-lg-10 col-md-10 col-sm-10 mt-5 p-2 w-100  shadow'>
-                    <Tabs
-                        defaultActiveKey="home"
-                        transition={false}
-                        id="noanim-tab-example"
-                        className="mb-3"
-                    >
-                        <Tab eventKey="home" title="Home">
-                        </Tab>
-                        <Tab eventKey="profile" title="Profile">
-                        </Tab>
-                        <Tab eventKey="contact" title="Contact">
-                        </Tab>
-                    </Tabs>
+                    <Nav  variant="pills" >
+                    <Nav.Item className={`p-3 ${Styles.link}`}>
+                            <Nav.Link href="#home">Program Details</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item className={`p-3 ${Styles.link}`}>
+                            <Nav.Link eventKey="link-1">Application Form</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item className={`p-3 ${Styles.link}`}>
+                            <Nav.Link eventKey="link-2">Workflow</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item className='p-3'>
+                            <Nav.Link eventKey="link-3">Preview</Nav.Link>
+                        </Nav.Item>
+                    </Nav>
                 </article>
                 <article className='p-5'>
                     <PhotoForm />
