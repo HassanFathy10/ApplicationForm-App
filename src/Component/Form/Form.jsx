959233655aacf807fc39c3082ceaab30a1a69aa5
Hassan Fathy <hassanfathy460@gmail.com>
2023-10-24 18:22:22 +0300

    .

--- src/Component/Form/Form.jsx
+++ src/Component/Form/Form.jsx
@@ -11,18 +11,18 @@ export default function Form() {
                 <article className='d-flex'>
                     <article className='w-100 mt-5 shadow'>
                         <article className=' p-2 shadow'>
-                            <Nav variant="pills" >
+                            <Nav variant="tabs">
                                 <Nav.Item className={`p-3 ${Styles.links}`}>
-                                    <Nav.Link variant="secondary" eventKey="link-1"><Link to='/' className={`text-decoration-none ${Styles.link}`}>Program Details</Link></Nav.Link>
+                                    <Nav.Link eventKey="link-1"><Link to='/' className={`text-decoration-none fw-bold ${Styles.link}`}>Program Details</Link></Nav.Link>
                                 </Nav.Item>
                                 <Nav.Item className={`p-3 ${Styles.links}`}>
-                                    <Nav.Link eventKey="link-2"><Link to='/ApplicationForm' className={`text-decoration-none ${Styles.link}`}>Application Form</Link></Nav.Link>
+                                    <Nav.Link eventKey="link-2"><Link to='/ApplicationForm' className={`text-decoration-none fw-bold ${Styles.link}`}>Application Form</Link></Nav.Link>
                                 </Nav.Item>
                                 <Nav.Item className={`p-3 ${Styles.links}`}>
-                                    <Nav.Link eventKey="link-3"><Link to='/Workflow' className={`text-decoration-none ${Styles.link}`}>Workflow</Link></Nav.Link>
+                                    <Nav.Link eventKey="link-3"><Link to='/Workflow' className={`text-decoration-none fw-bold ${Styles.link}`}>Workflow</Link></Nav.Link>
                                 </Nav.Item>
                                 <Nav.Item className='p-3'>
-                                    <Nav.Link eventKey="link-4"><Link to='/Preview' className={`text-decoration-none ${Styles.link}`}>Preview</Link></Nav.Link>
+                                    <Nav.Link eventKey="link-4"><Link to='/Preview' className={`text-decoration-none fw-bold ${Styles.link}`}>Preview</Link></Nav.Link>
                                 </Nav.Item>
                             </Nav>
                         </article>
