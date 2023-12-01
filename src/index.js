0d0886b2f13c9a855c03b6952249f57b0de44585
Hassan Fathy <118758238+HassanFathy10@users.noreply.github.com>
2023-12-01 11:10:31 +0200

    .

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import ErrorPage from './pages/ErrorPage';
 import ApplicationInfo from './pages/ApplicationInfo';
 import ProgramDetails from './pages/programDetails';
-import ApplicationForm from './pages/applicationform';
-import Workflow from './pages/workflow';
-import Preview from './pages/preview';
+import ApplicationForm from './pages/ApplicationForm';
+import Workflow from './pages/Workflow';
+import Preview from './pages/Preview';
 
 const router = createBrowserRouter([
   {
